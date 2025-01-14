949e808bae065156b3223574bd44fb3042b80f79
Harkaputra <143497383+hafizdharka@users.noreply.github.com>
2025-01-14 07:50:50 +0700

    Time: 703 ms (18.80%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select id,
-    case when mod(id,2)=0 then lag(student,1) over()
-    else coalesce(lead(student,1)over(),student)
-    end as student
-from seat
+SELECT id,
+    CASE WHEN MOD(id,2)=0 THEN LAG(student,1) OVER()
+        ELSE COALESCE(LEAD(student,1)OVER(),student)
+    END AS student
+FROM seat
