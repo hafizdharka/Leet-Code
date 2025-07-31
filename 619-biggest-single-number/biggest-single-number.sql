548d7fa59fd736df79d8af6fe51396cd3049aebc
Harkaputra <143497383+hafizdharka@users.noreply.github.com>
2025-07-31 16:17:50 +0700

    Time: 436 ms (39.81%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,9 @@
-# Write your MySQL query statement below
-select case
-    when exists (select num from MyNumbers group by num having count(num)=1 order by num desc)
-    then (select num from MyNumbers group by num having count(num)=1 order by num desc limit 1)
-    else null
-end as num
-    
+with cte as(
+    select num
+    from MyNumbers m1
+    group by m1.num
+    having count(*)=1
+)
+
+select max(num) as num
+from cte
