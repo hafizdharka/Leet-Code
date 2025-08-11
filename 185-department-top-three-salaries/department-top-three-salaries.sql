a3f128eac4462fd003f3004c14ba98a0e2b6baac
Harkaputra <143497383+hafizdharka@users.noreply.github.com>
2025-08-11 20:34:50 +0700

    Time: 1882 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,6 +1,14 @@
-SELECT D.name as Department, E.name as Employee, E.salary 
-FROM department D, employee E, employee E2  
-WHERE 
-    (D.ID = E.DepartmentId) AND (E.DepartmentId = E2.DepartmentId) AND (E.salary <= E2.salary)
-GROUP BY D.id,E.name HAVING COUNT(DISTINCT E2.salary) <= 3
-ORDER BY D.name, E.salary DESC
+WITH CTE AS(
+    SELECT
+        e.name Employee,
+        d.name Department,
+        e.salary Salary,
+        DENSE_RANK() OVER(PARTITION BY d.id ORDER BY e.salary DESC) as ranked
+    FROM employee e
+    LEFT JOIN department d
+    ON e.departmentId=d.id
+)
+
+SELECT Department, Employee, Salary
+FROM CTE
+WHERE ranked<=3
